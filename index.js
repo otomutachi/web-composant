e004720fc3a2d818b6a938bf1cfa3170c6dae605
Cédric Maviet <cedric.maviet@gmail.com>
2026-02-17 09:25:55 +1100

    maj

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
 class UiCard extends HTMLElement {
     constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+        super()
+        this.attachShadow({ mode: "open" });
         this.firstname = this.getAttribute("firstname");
-        this.firstname = this.getAttribute("firstname");
-        this.firstname = this.getAttribute("firstname");
-    
+        this.lastname = this.getAttribute("lastname");
+        this.email = this.getAttribute("email");
+
+        console.log("firstname : ", this.firstname)
+        console.log("lastname : ", this.lastname)
+        console.log("email : ", this.email)
     }
 
     connectedCallback() {
-        console.log('ui card ready')
+        console.log('Ui card ready');
         this.render();
     }
 
     render() {
-        const title = this.getAttribute('title') || 'Card Title';
-        const content = this.getAttribute('content') || 'Card content';
-        const footer = this.getAttribute('footer') || 'Card footer';
-        const image = this.getAttribute('image') || '';
-        
-        const imageHtml = image ? `<img src="${image}" alt="${title}">` : '';
+        this.shadowRoot.innerHTML = `
 
-        this.shadowRoot.innerHTML =` 
-        <div>
-             <header
-        <div>`
+            <style>
+                :host {
+                    width: calc(33.333% - 14px);
+                }
 
-        const template = `
-            <div class="card">
-                ${imageHtml}
-                <h2>${title}</h2>
-                <p>${content}</p>
-                <footer>${footer}</footer>
+                div {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+
+                    background: #222;
+                    padding: 20px;
+                    border-radius: 20px;
+
+                    color: white;
+                }
+
+                img {
+                    max-width:100%;
+                }
+            </style>
+
+            <div>
+                <header>
+                    <img src="https://images4.fanpop.com/image/photos/17000000/Anakin-Skywalker-anakin-skywalker-17028586-992-960.jpg" />
+                </header>
+                <article>
+                    <p>Prénom : <strong>${this.firstname}</strong></p>
+                    <p>Nom : <strong>${this.lastname}</strong></p>
+                    <p>Email : <strong>${this.email}</strong></p>
+                </article>
             </div>
-        `;
-        
-        this.shadowRoot.innerHTML =  template;
+        `
     }
 }
 
-customElements.define('ui-card', UiCard);
+customElements.define("ui-card", UiCard);
